d3f4f11dabb915e6ffe7f0fe6d641d251dbeb9c4
Tobi <tobias@verhulst.fr>
2017-04-26 18:11:30 +0200

    Added printing of matrix stats

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ NB_USERS = 10000
 NB_ITEMS = 1000
 
 def load_data(filename):
+    print("Loading data...")
     with open(filename, 'r') as f:
         lines = f.readlines()
         
@@ -31,6 +32,7 @@ def load_data(filename):
     return values, rows, cols
 
 def write_data(filename, matrix):
+    print("Writing to file...")     
     with open(filename, 'w') as write_file:
         write_file.write("Id,Prediction\n")
         with open(SUBMISSION_EXAMPLE, 'r') as example_file:
@@ -45,6 +47,11 @@ def write_data(filename, matrix):
                 
                 write_file.write(SUBMISSION_FORMAT.format(user+1, item+1,matrix[item,user]))
 
+def print_stats(matrix):
+    print("The matrix has the following values:")
+    print("Values range from {} to {}".format(np.min(matrix),np.max(matrix)))
+    print("Average value is {}".format(np.average(matrix)))
+    
 values, rows, cols = load_data(DATA_FILE)
 
 data = scipy.sparse.csr_matrix((values, (rows, cols)), shape=(NB_ITEMS, NB_USERS), dtype='float')
@@ -61,7 +68,7 @@ average_for_item = np.true_divide(predictions.sum(1), (predictions!=0).sum(1))
 for i in range(predictions.shape[0]):
     predictions[i,:] = np.where(predictions[i,:] == 0, average_for_item[i], predictions[i,:])
                 
-K = 50
+K = 10
 print("Computing SVD...")
 U, S, VT = np.linalg.svd(predictions, full_matrices=True)
 
@@ -70,7 +77,7 @@ S2 = S[:K]
 VT2 = VT[:K, :]
 
 reduced_data = U2.dot(np.diag(S2)).dot(VT2)
-       
-print("Writing to file...")     
+      
+print_stats(reduced_data) 
 filename = TARGET_FOLDER + "/submission_{}_{}.csv".format(USERNAME, time.strftime("%c").replace(":","-")[4:-5])
 write_data(filename, reduced_data)
