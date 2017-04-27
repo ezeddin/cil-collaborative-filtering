4bdd79b44fabec1f169e3964325de6d35a4e57c0
Cyril Stoller <stollerc@student.ethz.ch>
2017-04-27 11:01:28 +0200

    added local validation possibility and structured training to be able to select different models

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ NB_USERS = 10000
 NB_ITEMS = 1000
 
 DO_LOCAL_VALIDATION = True
-MODEL = 'HYPER_SVD'
-
+MODEL = 'SVD'
+INJECT_TEST_DATA = False
 ROUND = 0
 
 def load_data(filename):
@@ -42,6 +42,18 @@ def load_data(filename):
     print('average rating : {}'.format( data.sum() / nb_ratings))
     return data.todense(), nb_ratings
 
+def build_validation_set(raw_data):
+    # randomly sample half of the non-zero values and define them as the new secret set
+    non_zero_indices = list(zip(*np.nonzero(raw_data)))
+    np.random.shuffle(non_zero_indices)
+    secret_set_indices = non_zero_indices[:nb_ratings//2]
+    known_set_indices = non_zero_indices[nb_ratings//2:]
+    training_data = np.array(raw_data)
+    training_data[list(zip(*secret_set_indices))] = 0
+    secret_data = np.array(raw_data)
+    secret_data[list(zip(*known_set_indices))] = 0
+    return training_data, secret_data
+
 def write_data(filename, matrix):
     with open(filename, 'w') as write_file:
         write_file.write('Id,Prediction\n')
@@ -55,7 +67,8 @@ def write_data(filename, matrix):
                 user = int(c[0][1:]) - 1 # 0-based indexing
                 item = int(c[1][1:]) - 1 # 0-based indexing
                 
-                write_file.write(SUBMISSION_FORMAT.format(user+1, item+1, np.round(matrix[item,user], ROUND)))
+                rating = max(0, min(5, np.round(matrix[item,user], ROUND)))
+                write_file.write(SUBMISSION_FORMAT.format(user+1, item+1, rating))
 
 def average_prediction(matrix):
     average_for_item = np.true_divide(matrix.sum(1), (matrix!=0).sum(1))
@@ -72,7 +85,7 @@ def svd_prediction(matrix, K=15):
     return U2.dot(np.diag(S2)).dot(VT2)
 
 def sgd_prediction(matrix):
-    return matrix
+    return np.array(matrix)
 
 def validate(data, training_data, secret_data, approximation):
     row_errors = np.zeros((data.shape[0],))
@@ -80,29 +93,24 @@ def validate(data, training_data, secret_data, approximation):
         row_errors[i] = np.where(secret_data[i,:] != 0 , np.square(approximation[i,:] - secret_data[i,:]), 0).sum()
     return row_errors.sum() / (training_data!=0).sum()
 
+
+# load data from file
 print('Loading data file...')
 raw_data, nb_ratings = load_data(DATA_FILE)
+if INJECT_TEST_DATA:
+    raw_data = np.array([
+        [0, 0, 5, 4, 0, 0],
+        [0, 2, 0, 0, 0, 1],
+        [6, 0, 0, 0, 8, 0],
+        [0, 2, 1, 0, 0, 0],
+        ])
+    nb_ratings = (raw_data!=0).sum()
 
-#raw_data = np.array([
-#    [0, 0, 5, 4, 0, 0],
-#    [0, 2, 0, 0, 0, 1],
-#    [6, 0, 0, 0, 8, 0],
-#    [0, 2, 1, 0, 0, 0],
-#    ])
-#nb_ratings = (raw_data!=0).sum()
 
 # prepare training set
 if DO_LOCAL_VALIDATION:
-    # randomly sample half of the non-zero values and define them as the new secret set
     print('Building validation set...')
-    non_zero_indices = list(zip(*np.nonzero(raw_data)))
-    shuffled_indices = np.random.shuffle(non_zero_indices)
-    secret_set_indices = non_zero_indices[:nb_ratings//2]
-    known_set_indices = non_zero_indices[nb_ratings//2:]
-    training_data = np.array(raw_data)
-    training_data[list(zip(*secret_set_indices))] = 0
-    secret_data = np.array(raw_data)
-    secret_data[list(zip(*known_set_indices))] = 0
+    training_data, secret_data = build_validation_set(raw_data)
 else:
     training_data = raw_data
 
@@ -111,10 +119,8 @@ else:
 print('Running Model {}...'.format(MODEL))
 if MODEL == 'average':
     predictions = average_prediction(training_data)
-
 elif MODEL == 'SVD':
-    predictions = svd_prediction(average_prediction(training_data), K=15)
-
+    predictions = svd_prediction(average_prediction(training_data), K=12)
 elif MODEL == 'HYPER_SVD': 
     # try SVD with different K values
     Ks = list(range(4, 20))
@@ -135,11 +141,11 @@ elif MODEL == 'HYPER_SVD':
             scores[i][epoch] = validate(raw_data, training_data, secret_data, svd_prediction(average_prediction(training_data), K=K))
     pickle.dump(scores, open('scores.pkl', 'wb'))
     plt.plot(Ks,np.average(scores, axis=1))
-            
-    
 elif MODEL == 'SGD':
     predictions = sgd_prediction(training_data)
 
+
+# validate locally or export prediction for submission
 if MODEL != 'HYPER_SVD':
     # either do local score computation or write submission
     if DO_LOCAL_VALIDATION:
