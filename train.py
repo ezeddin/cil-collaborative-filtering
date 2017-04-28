a4f151d8bd03b1fb74a2ceab4451c453a3e634dd
Cyril Stoller <stollerc@student.ethz.ch>
2017-04-28 13:00:51 +0200

    added learning rate and n_iter to global argument parser

--- train.py
+++ train.py
@@ -39,6 +39,10 @@ def main():
                         help='Data splits for cross validation ')
     parser.add_argument('--param', type=str, default="10",
                         help='Hyper parameter, can also be a list')
+    parser.add_argument('--learning_rate', type=float, default=0.01,
+                        help='Learning rate of SGD')
+    parser.add_argument('--n_iter', type=float, default=60e6,
+                        help='Number of iterations for SGD')
     parser.add_argument('--postproc', type=bool, default=True,
                         help='Do post procession like range cropping')
     parser.add_argument('--v', type=int, default=2,
@@ -226,7 +230,7 @@ def run_model(training_data, param1):
     elif args.model == 'SVD2':
         predictions = svd_prediction(sampling_distribution_fill_up(training_data), K=param1)
     elif args.model == 'SGD':
-        predictions = sgd_prediction(training_data, K=param1, verbose=args.v)
+        predictions = sgd_prediction(training_data, K=param1, learning_rate_factor=args.learning_rate, n_iter=arg.n_iter, verbose=args.v)
     if args.postproc:
         post_process(predictions)
     return predictions
