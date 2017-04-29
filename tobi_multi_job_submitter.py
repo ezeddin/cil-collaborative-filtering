b7ee5ca3110ad6881ea50a23ac741de367674099
Tobi <tobias@verhulst.fr>
2017-04-29 18:49:21 +0200

    Added tobi's euler multi_job_submitter

---
+++ tobi_multi_job_submitter.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+
+K = list(range(20, 41, 5))
+lr_factors = [0.1, 0.2, 0.5 , 0.8 , 1.0, 1.2, 1.5, 2]
+
+submission = 'bsub -n 1 -W 00:30 -R "rusage[mem=700]" "python3 train.py --cv_splits=8 --param={} --L=0.084 --lr_factor={} "'
+
+if input('Delete all previous score files? (y/n) ') == 'y':
+    os.system('rm data/scores_*')
+
+print('Starting submitting jobs:')
+i = 0
+for k in K:
+    k_list = '[{}]'.format(k)
+    for lrf in lr_factors:
+	    print('Submitting job #{:02} for K={}, lrf={}: {}'.format(i, k_list, lrf,  submission.format(k_list, lrf)))
+	    os.system(submission.format(k_list, lrf))
+	    i += 1
