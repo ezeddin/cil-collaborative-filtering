6d4923b531da038a872609208065ae52c59f9ec7
Tobi <tobias@verhulst.fr>
2017-04-29 23:11:16 +0200

    Changed the multi job submitter for tuning the new hyper parameter (L2) on euler

--- tobi_multi_job_submitter.py
+++ tobi_multi_job_submitter.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
 
-K = list(range(20, 41, 5))
-lr_factors = [0.8 , 1, 1.5, 2, 2.5, 3, 3.5]
+L=0.084
+K = [3, 6, 10, 15, 20]
+L2s = [0.05,0.08,0.1, 0.12, 0.15, 0.18, 0.20, 0.25, 0.30]
 
-submission = 'bsub -n 1 -W 00:30 -R "rusage[mem=700]" "python3 train.py --cv_splits=8 --param={} --L=0.084 --lr_factor={} "'
+
+submission = 'bsub -n 1 -W 00:30 -R "rusage[mem=700]" "python3 train.py --param={} --L=0.084 --L2={} "'
 
 if input('Delete all previous score files? (y/n) ') == 'y':
     os.system('rm data/scores_*')
@@ -13,7 +15,7 @@ print('Starting submitting jobs:')
 i = 0
 for k in K:
     k_list = '[{}]'.format(k)
-    for lrf in lr_factors:
-	    print('Submitting job #{:02} for K={}, lrf={}: {}'.format(i, k_list, lrf,  submission.format(k_list, lrf)))
+    for l2 in L2s:
+	    print('Submitting job #{:02} for K={}, L2={}: {}'.format(i, k_list, l2,  submission.format(k_list, lrf)))
 	    os.system(submission.format(k_list, lrf))
 	    i += 1
