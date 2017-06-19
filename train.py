ac2f657f738dcbf0afd9470dc71791a50be7d91e
Tobi <tobias@verhulst.fr>
2017-06-19 17:14:41 +0200

    Deleted SVD2

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ def main(arguments, matrix=None):
     parser.add_argument('--submission', type=bool, default=False,
                         help='Omit local validation, just export submission file.')
     parser.add_argument('--model', type=str, default='SGD+',
-                        help='Prediction algorithm: average, SVD, SVD2, SGD, SGD+, SGDnn')
+                        help='Prediction algorithm: average, SVD, SGD, SGD+, SGDnn')
     parser.add_argument('--cv_splits', type=int, default=14,
                         help='Number of data splits for cross validation')
     parser.add_argument('--score_averaging', type=int, default=1,
@@ -109,7 +109,6 @@ def load_data(filename):
     print('average rating : {}'.format( data.sum() / nb_ratings))
     return np.asarray(data.todense())
 
-
 def write_data(filename, matrix):
     print("Writing to file...")     
     with open(filename, 'w') as write_file:
@@ -125,15 +124,13 @@ def write_data(filename, matrix):
                 item = int(c[1][1:]) - 1 # 0-based indexing
                 
                 rating = np.round(matrix[item,user], ROUND)
-                write_file.write(SUBMISSION_FORMAT.format(user+1, item+1, rating))
-                
+                write_file.write(SUBMISSION_FORMAT.format(user+1, item+1, rating))          
     
 def print_stats(matrix):
     print("The matrix has the following values:")
     print("  > Values range from {} to {}".format(np.min(matrix),np.max(matrix)))
     print("  > Average value is {}".format(np.average(matrix)))
     
-
 def split_randomly(raw_data, n_splits=8):
     """
         randomly splits the non-zero indices of the raw_data matrix into n parts. 
@@ -145,7 +142,6 @@ def split_randomly(raw_data, n_splits=8):
     assert(size * n_splits == len(non_zero_indices)), "n chosen for cross validation does not evenly split data. Choose 4, 7, 8, 14, 28, 56"
     
     return [non_zero_indices[(a*size): ((a+1)*size)] for a in range(n_splits)]
-
 
 def build_train_and_test(raw_data, index_splits, use_as_test):
     """
@@ -166,7 +162,6 @@ def build_train_and_test(raw_data, index_splits, use_as_test):
     assert(np.array_equal(train_data+test_data, raw_data))
     return train_data, test_data
 
-
 def averaging_fill_up(matrix):
     """
         fill up the sparse matrix row by row with the average value of this row
@@ -176,21 +171,6 @@ def averaging_fill_up(matrix):
     for i in range(matrix.shape[0]):
         averaged[i,:] = np.where(averaged[i,:] == 0, average_for_item[i], averaged[i,:])
     return averaged
-
-
-def sampling_distribution_fill_up(matrix):
-    """
-        fill up the sparse matrix row by row by sampling from the empirical distribution
-        of the rating in this row.
-    """
-    filled = np.array(matrix)
-    for i in range(matrix.shape[0]):
-        nonzeros_per_row = np.squeeze(matrix[i, np.where(matrix[i,:] != 0.0)[0]])
-        for j in range(matrix.shape[1]):
-            if filled[i,j] == 0:
-                filled[i,j] = np.random.choice(nonzeros_per_row, replace=True)
-    return filled
-
 
 def svd_prediction(matrix, K=15):
     """
@@ -203,8 +183,6 @@ def svd_prediction(matrix, K=15):
     S2 = S[:K]
     VT2 = VT[:K, :]
     return U2.dot(np.diag(S2)).dot(VT2)
-
-
 
 def retrain_U(matrix, test_data, V):
     K = V.shape[1]
@@ -395,8 +373,6 @@ def run_model(training_data, test_data, K):
         predictions = averaging_fill_up(training_data)
     elif args.model == 'SVD':
         predictions = svd_prediction(averaging_fill_up(training_data), K=K)
-    elif args.model == 'SVD2':
-        predictions = svd_prediction(sampling_distribution_fill_up(training_data), K=K)
     elif args.model == 'SGD':
         predictions = sgd_prediction(training_data, test_data, K=K, verbose=args.v, L=args.L, L2=args.L2, save_model=args.save_model,model_path=args.model_path, use_bias=False)
     elif args.model == 'SGD+':
